c4862b25de41b19d7e76a6604f5f53057b37c5db
hellomartelle <10738274@mackenzista.com.br>
2025-10-29 11:33:57 +0000

    .

---
+++ Aula 12/atv.2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void decimal_para_binario(int n) {
+    if (n > 0) {
+        decimal_para_binario(n / 2);
+        printf("%d", n % 2);
+    }
+}
+
+int main() {
+    int numero;
+
+    printf("Digite um numero inteiro positivo: ");
+    scanf("%d", &numero);
+
+    if (numero < 0) {
+        printf("Por favor, digite um numero positivo.\n");
+    } else if (numero == 0) {
+        printf("0");
+    } else {
+        printf("O numero %d em binario eh: ", numero);
+        decimal_para_binario(numero);
+    }
+
+    printf("\n");
+    return 0;
+}
